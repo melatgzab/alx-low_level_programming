d1c93cf494c1a3628ad8fd4b0ccb716ce2c0f985
melatgzab <myemail@gmail.com>
2023-02-16 13:37:37 -0800

    4 puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 
 /**
  * main - prints "Programming is like building a multiilingual 
- * puzzle, followed by new line."
+ * puzzle, followed by newline.
  * Return: Always 0.
  */
 
@@ -11,5 +11,5 @@ int main(void)
 
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
