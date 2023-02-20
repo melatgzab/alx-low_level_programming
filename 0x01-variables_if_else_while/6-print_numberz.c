e1995743079bc39a03674985c4166f346ce7cbbe
melatgzab <myemail@gmail.com>
2023-02-20 03:31:35 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - display start from 0
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int c = 0;
+
+	while (c < 10)
+	{
+		putchar(48 + c c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
