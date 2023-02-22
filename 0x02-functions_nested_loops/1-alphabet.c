fc3857d4885bfd9f14a9496e84c3e62127e8e84f
melatgzab <myemail@gmail.com>
2023-02-22 13:13:24 -0800

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,13 +4,13 @@
  *
  * Return: void
  */
-void print_alphabet(void)
+void print_alphabet(void);
 {
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
 	{
-	 _putchar(c);
+		_putchar(c);
 	}
 	_putchar('\n');
 }
