8317732098efc6983f5d33e246d967df7e21911b
melatgzab <myemail@gmail.com>
2023-02-22 14:04:00 -0800

    1 if lower case 0 if nott

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _islower - checks for lowercase letter
+ * @c: the character to be checked
+ * Return: 1 if lowercase 0 if not
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+	return (1);
+	}
+	return (0);
+}
+
+
