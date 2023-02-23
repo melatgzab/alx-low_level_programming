36b3b42a11c67e357ee3e266af6f28f6c9bf5802
melatgzab <myemail@gmail.com>
2023-02-23 05:45:02 -0800

    uppercase

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - writes c to stdout
+ * @c : character to print
+ *
+ * Return: 1 on error -1
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
